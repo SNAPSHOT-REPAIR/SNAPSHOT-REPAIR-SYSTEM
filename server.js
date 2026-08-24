6a41f0422f613c5119bc45c5f0a52a779d6a695a
SNAPSHOT_REPAIR <snapshotcomputer19@gmail.com>
2026-08-24 10:16:31 +0530

    add google index

--- server.js
+++ server.js
@@ -111,6 +111,25 @@ app.get("/", (req, res) => {
     res.render("index");
 });
 
+app.get("/sitemap.xml", (req, res) => {
+
+    res.header("Content-Type", "application/xml");
+
+    res.send(`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
+    <url>
+        <loc>https://snapshotrepair.in/</loc>
+        <changefreq>weekly</changefreq>
+        <priority>1.0</priority>
+    </url>
+
+
+</urlset>`);
+
+});
+
+
 app.get("/engineer/login", (req, res) => {
 
     res.render("engineer-login", {
